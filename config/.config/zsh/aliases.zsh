5a197a4c2e2d908bb12abb3d4afb08fa1b616ccd
Joseph Hansen <56806498+Artach-byte@users.noreply.github.com>
2024-03-02 02:36:19 -0800

    updated config

--- config/.config/zsh/aliases.zsh
+++ config/.config/zsh/aliases.zsh
@@ -1,11 +1,22 @@
 # Aliases for ZSH
 alias zshreload='source ~/.zshrc' # reload ZSH
 
-# back directory
-alias ..='cd ..'
+# Dirs
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
 
 # ls
 alias ls='ls --color'
 
+# Eza
+alias l="eza -l --icons --git -a"
+alias lt="eza --tree --level=2 --long --icons --git"
+
 # lazygit
 alias lg='lazygit'
+
+#filemanager
+alias fm='yazi'

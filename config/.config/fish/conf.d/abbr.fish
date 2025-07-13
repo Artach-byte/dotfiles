9ecf9d61047fe6e8d147241e384bb71a2d3eec83
Joseph Hansen <56806498+Artach-byte@users.noreply.github.com>
2025-07-13 15:24:46 -0700

    removed fish

--- config/.config/fish/conf.d/abbr.fish
+++
@@ -1,4 +0,0 @@
-abbr fm yazi
-abbr lg lazygit
-abbr l eza -l --icons --git -a -G
-abbr lt eza --tree --level=2 --long --icons --git
--- config/.config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-    set -gx EDITOR hx
-    set -Ux EDITOR hx
-    atuin init fish | source
-end

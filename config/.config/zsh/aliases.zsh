e1810b46fce11913a9a4f1e80582e01221dd26fd
Joseph Hansen <56806498+Artach-byte@users.noreply.github.com>
2025-07-13 15:33:04 -0700

    updated alias and .zshrc

--- config/.config/zsh/aliases.zsh
+++ config/.config/zsh/aliases.zsh
@@ -1,6 +1,9 @@
 # Aliases for ZSH
 alias zshreload='source ~/.zshrc' # reload ZSH
 
+#alias ls='ls --color'
+alias ls='eza -l --icons --git -a -G'
+
 # Directory navigation
 alias ..="cd .."
 alias ...="cd ../.."

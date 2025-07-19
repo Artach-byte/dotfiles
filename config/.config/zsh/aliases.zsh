abb463b63b6a94b0e432e6fc3a314d69961ba6d5
Joseph Hansen <56806498+Artach-byte@users.noreply.github.com>
2025-07-19 16:26:04 -0700

    cleaned up zsh config

--- config/.config/zsh/aliases.zsh
+++ config/.config/zsh/aliases.zsh
@@ -3,17 +3,9 @@ alias zshreload='source ~/.zshrc' # reload ZSH
 
 alias ls='ls --color'
 
-alias ls='eza -A --grid --group-directories-first --icons'
-
 # Directory navigation
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
-
-# Git tools
-alias lg='lazygit'
-
-# File manager
-alias fm='yazi'
